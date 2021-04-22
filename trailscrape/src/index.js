c38dc5206e62a5642512c983c5b0efae6559cc01
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-04-23 09:54:56 +1200

    add footer

--- trailscrape/src/index.js
+++ trailscrape/src/index.js
@@ -5,5 +5,9 @@ import './index.css';
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
 
-//TODO:
-//reroute region url to get trail info
+// TODO:
+// add api support for routing directly to subpage
+// page view for a unavailable park?
+// 404
+// footer
+// seo
