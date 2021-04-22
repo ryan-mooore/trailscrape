a8f4e856846d3bd858b5d34f25ad9f395401d2cf
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-04-23 10:10:47 +1200

    removing flex-gap for safari support

--- trailscrape/src/components/shared/card.jsx
+++ trailscrape/src/components/shared/card.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Card = (props) => {
   const style =
-    "bg-white rounded-3xl sm:rounded-full overflow-hidden ml-5 mr-5 pt-2 pb-2 sm:pt-0 sm:pb-0";
+    "bg-white rounded-3xl sm:rounded-full overflow-hidden ml-5 mr-5 pt-2 pb-2 mt-3 sm:pt-0 sm:pb-0";
   const layout = (
     <div className="sm:flex flex-row justify-between sm:h-14 pl-5 pr-2">
       <div className="flex flex-row justify-start items-center">
--- trailscrape/src/components/shared/status-info.jsx
+++ trailscrape/src/components/shared/status-info.jsx
@@ -2,10 +2,10 @@ import StatusBadge from "./status-badge";
 
 const StatusInfo = (props) => {
   return (
-    <div className="flex flex-row gap-6">
+    <div className="flex flex-row">
       {Object.entries(props.status).map(([k, v]) => (
-        <div key={k} className="flex flex-row gap-3 items-center text-gray-400">
-          <div className="invisible xs:visible">{k}</div>
+        <div key={k} className="pl-6 flex flex-row items-center text-gray-400">
+          <div className="invisible xs:visible pr-3">{k}</div>
           <div className={k === "lift" ? "invisible xs:visible" : undefined}>
             <StatusBadge open={v} />
           </div>
