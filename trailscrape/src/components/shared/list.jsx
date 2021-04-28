52269c4becf922360d70c3e9df1dd468f02e1f3f
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-04-28 21:31:37 +1200

    better footer

--- trailscrape/src/components/shared/list.jsx
+++ trailscrape/src/components/shared/list.jsx
@@ -1,6 +1,6 @@
 const List = (props) => {
   return (
-    <div className="flex flex-col max-w-md sm:max-w-screen-md m-auto items-stretch pb-14">
+    <div className="flex flex-col max-w-md sm:max-w-screen-md m-auto items-stretch pb-12">
       {props.disclaimer ? (
         <div className="pl-10 pr-10 text-gray-400 text-xs italic">
           {props.disclaimer}
