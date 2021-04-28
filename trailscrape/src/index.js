960bb35d428bd3d19646bdd576c218db99d4ff00
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-04-28 21:58:00 +1200

    seo

--- trailscrape/src/index.js
+++ trailscrape/src/index.js
@@ -1,10 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes'
-import React from 'react'
 import './index.css';
-import { Router } from 'react-router-dom';
+import App from './routes';
 
-import App from './routes'
 
 ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
 
