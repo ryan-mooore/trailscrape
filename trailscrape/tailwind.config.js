02552562dec1a9e18208229aab40b36fa33f177d
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-04-28 22:19:53 +1200

    adding favicons, more seo, cleaning up tailwind.config.js

--- trailscrape/tailwind.config.js
+++ trailscrape/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['montserrat'],
+      'sans': ['montserrat', defaultTheme.fontFamily.sans]
     },
       screens: {
         'xs': '380px',
