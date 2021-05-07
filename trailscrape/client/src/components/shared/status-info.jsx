f4bd79fe2a0b19324079f3217883af5e12558116
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-05-07 14:01:35 +1200

    fix border issue

--- trailscrape/client/src/components/shared/status-info.jsx
+++ trailscrape/client/src/components/shared/status-info.jsx
@@ -2,9 +2,9 @@ import StatusBadge from "./status-badge";
 
 const StatusInfo = (props) => {
   return (
-    <div className="flex flex-col items-end xs:flex-row">
+    <div className="flex flex-col items-end sm:items-center xs:flex-row">
       {Object.entries(props.status).map(([k, v]) => (
-        <div key={k} className="pl-6 flex pt-3 flex-row items-center text-gray-400">
+        <div key={k} className="pl-6 flex pt-3 sm:pt-0 flex-row items-center text-gray-400">
           <div className="pr-3">{k}</div>
           <div>
             <StatusBadge open={v} />
