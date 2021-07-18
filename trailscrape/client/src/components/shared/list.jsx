ee075f6ade05cd645e01e337600df4e64a11731e
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-07-19 00:00:04 +1200

    fix dynamic grid cols issue

--- trailscrape/client/src/components/shared/list.jsx
+++ trailscrape/client/src/components/shared/list.jsx
@@ -1,4 +1,20 @@
 const List = ({ disclaimer, elements, cols }) => {
+  let gridCols;
+  console.log(cols);
+  switch (cols) {
+    case 1:
+      gridCols = "grid-cols-1";
+      break;
+    case 2:
+      gridCols = "grid-cols-2";
+      break;
+    case 3:
+      gridCols = "grid-cols-3";
+      break;
+    default:
+      gridCols = "grid-cols-1";
+  }
+
   return (
     <div className="pl-5 pr-5 flex flex-col max-w-md sm:max-w-screen-lg m-auto items-stretch sm:pb-12">
       {disclaimer ? (
@@ -6,7 +22,7 @@ const List = ({ disclaimer, elements, cols }) => {
           {disclaimer}
         </div>
       ) : undefined}
-      <div className={`grid md:grid-cols-${cols} gap-3`}>{elements}</div>
+      <div className={`grid md:${gridCols} gap-3`}>{elements}</div>
     </div>
   );
 };
