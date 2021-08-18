a6c8d9a76c7d897a5d4b260b91242adf86a37869
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-08-18 21:35:00 +1200

    removed unnecessary api views

--- trailscrape/api/app.js
+++ trailscrape/api/app.js
@@ -7,11 +7,6 @@ var trailsRouter = require('./routes/api');
 
 var app = express();
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
