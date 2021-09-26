16b5760033f2589597e9abc9e3762e0f8ce69800
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-09-26 20:08:57 +1300

    added proline icon

---
+++ trailscrape/client/src/assets/icons/grade/7.jsx
@@ -0,0 +1,39 @@
+// <?xml version="1.0" encoding="utf-8"?>
+// <!-- Generator: Adobe Illustrator 25.4.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+// <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+// 	 viewBox="0 0 400 400" style="enable-background:new 0 0 400 400;" xml:space="preserve">
+// <style type="text/css">
+// 	.st0{fill:#EB2A2E;}
+// </style>
+// <g>
+// 	<circle class="st0" cx="200" cy="200" r="170"/>
+// 	<path d="M200,60c37.4,0,72.6,14.6,99,41c26.4,26.4,41,61.6,41,99s-14.6,72.6-41,99c-26.4,26.4-61.6,41-99,41s-72.6-14.6-99-41
+// 		c-26.4-26.4-41-61.6-41-99s14.6-72.6,41-99C127.4,74.6,162.6,60,200,60 M200,0C89.5,0,0,89.5,0,200s89.5,200,200,200
+// 		s200-89.5,200-200S310.5,0,200,0L200,0z"/>
+// </g>
+// <g>
+// 	<g>
+// 		<path d="M200,145.5l47.2,81.8h-94.4L200,145.5 M200,25.5L48.9,287.3h302.3L200,25.5L200,25.5z"/>
+// 	</g>
+// </g>
+// </svg>
+
+const Grade7 = (props) => {
+  return (
+    <svg width={props.size} height={props.size} viewBox="0 0 400 400">
+      <circle class="st0" cx="200" cy="200" r="170" fill="#EB2A2E" />
+      <path
+        d="M200,60c37.4,0,72.6,14.6,99,41c26.4,26.4,41,61.6,41,99s-14.6,72.6-41,99c-26.4,26.4-61.6,41-99,41s-72.6-14.6-99-41
+		c-26.4-26.4-41-61.6-41-99s14.6-72.6,41-99C127.4,74.6,162.6,60,200,60 M200,0C89.5,0,0,89.5,0,200s89.5,200,200,200
+		s200-89.5,200-200S310.5,0,200,0L200,0z"
+        fill="#2b2b2b"
+      />
+      <path
+        d="M200,145.5l47.2,81.8h-94.4L200,145.5 M200,25.5L48.9,287.3h302.3L200,25.5L200,25.5z"
+        fill="#2b2b2b"
+      />
+    </svg>
+  );
+};
+
+export default Grade7;
